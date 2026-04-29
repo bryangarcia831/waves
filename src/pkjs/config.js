19fd97e1322a3c52e2be7a2c8fbbaabdd39abc0d
Bryan Garcia <bryangarcia831@gmail.com>
2026-04-28 18:15:31 -0700

    feat: Clay config page for seconds toggle

---
+++ src/pkjs/config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  { "type": "heading", "defaultValue": "Casio Waves" },
+  {
+    "type": "toggle",
+    "messageKey": "SHOW_SECONDS",
+    "label": "Show Seconds",
+    "description": "Ticks every second when on.",
+    "defaultValue": false
+  },
+  { "type": "submit", "defaultValue": "Save" }
+];
